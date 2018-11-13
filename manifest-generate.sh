4738b9176b396d1284d38a18d902c45ba404fb03
Holger Imbery <holger@imbery.de>
2018-11-13 07:27:22 +0100

    Update manifest-generate.sh

--- manifest-generate.sh
+++ manifest-generate.sh
@@ -1,4 +1,4 @@
 ./manifest-tool --username $1 --password $2 push from-spec manifest.yaml
 ./manifest-tool --username $1 --password $2 push from-spec manifest-slim.yaml
-./manifest-tool --username $1 --password $2 push from-spec manifest-jessie.yaml
-./manifest-tool --username $1 --password $2 push from-spec manifest-jessie-slim.yaml
+# ./manifest-tool --username $1 --password $2 push from-spec manifest-jessie.yaml
+# ./manifest-tool --username $1 --password $2 push from-spec manifest-jessie-slim.yaml
